070a5bd49c5da00a3b2afe54331f2929d239bb34
heath smith <heathsmith73@gmail.com>
2018-03-20 15:35:54 -0700

    update the front end with new search

--- src/component/content/index.js
+++ src/component/content/index.js
@@ -3,16 +3,27 @@ import {connect} from 'react-redux';
 import LowFareForm from '../lowfare-form/index';
 import FlightItem from '../flight-item/index';
 import * as lowFareActions from '../../action/lowfare-actions';
-import {renderIf} from '../../lib/utils';
+import * as inspirationAction from '../../action/inspiration-actions';
+import * as profileActions from '../../action/profile-actions';
+import InspirationForm from '../inspiration-form/index';
+
 
 class Content extends React.Component {
+
+  componentWillMount() {
+    this.props.getMyProfile();
+  }
 
   render() {
     return (
       <div>
         <h1>Get The Flight Out!</h1>
-        {console.log('$$$$$$$$$$$$$$$',this.props.lowFareSearch)}
         <LowFareForm onComplete={this.props.lowSearch}/>
+
+        <InspirationForm
+          onComplete={this.props.inspirationSearch}
+          profile={this.props.profile} />
+
         {this.props.lowFareSearch.length > 0 ?
           this.props.lowFareSearch[0].map(flight =>
             <div className="images" key={flight.fare.total_price}>
@@ -28,10 +39,13 @@ class Content extends React.Component {
 
 let mapSetToProps = state => ({
   lowFareSearch: state.lowFareSearch,
+  profile: state.profile,
 });
 
 const mapDispatchToProps = dispatch => ({
   lowSearch : search => dispatch(lowFareActions.lowFareSearchAction(search)),
+  inspirationSearch : search => dispatch(inspirationAction.inspirationAction(search)),
+  getMyProfile : profile => dispatch(profileActions.getMeProfileRequest(profile)),
 });
 
 export default connect(mapSetToProps, mapDispatchToProps)(Content);
