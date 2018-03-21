c261a31ed482b852ded773757733ac329a92aaad
Victoria Oh <veoh1989@gmail.com>
2018-03-20 18:20:01 -0700

    removed state from searches

--- src/reducer/inspiration.js
+++ src/reducer/inspiration.js
@@ -3,6 +3,8 @@ export default (state = [], {type, payload}) => {
   switch (type) {
   case 'INSPIRATION_SEARCH':
     return [payload, ...state];
+  case 'LOW_FARE_SEARCH':
+    return delete state.data;
   default:
     return state;
   }
--- src/reducer/lowfare.js
+++ src/reducer/lowfare.js
@@ -3,6 +3,8 @@ export default (state = [], {type, payload}) => {
   switch (type) {
   case 'LOW_FARE_SEARCH':
     return [payload, ...state];
+  case 'INSPIRATION_SEARCH':
+    return delete state.data;
   default:
     return state;
   }
