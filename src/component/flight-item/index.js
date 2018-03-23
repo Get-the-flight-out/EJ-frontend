77557fa04f801125efc1d13828359e0cd0b320fb
heath smith <heathsmith73@gmail.com>
2018-03-23 00:58:59 -0700

    fixed the images boxes and made some stuff look SH&T hot

--- src/component/flight-item/index.js
+++ src/component/flight-item/index.js
@@ -5,36 +5,34 @@ class FlightItem extends React.Component {
 
   render() {
     let retDate;
+    let part1;
     let image = `../../../src/assets/airport-img/${this.props.lowFareSearch.itineraries[0].outbound.flights[0].destination.airport}.jpg`;
     let origin = localStorage.getItem('origin');
     let airport = this.props.lowFareSearch.itineraries[0].outbound.flights[0].destination.airport;
     let city = airportLookup(this.props.lowFareSearch.itineraries[0].outbound.flights[0].destination.airport);
     let depDate = this.props.lowFareSearch.itineraries[0].outbound.flights[0].departs_at;
     this.props.lowFareSearch.itineraries[0].inbound ? retDate = this.props.lowFareSearch.itineraries[0].inbound.flights[0].departs_at : undefined;
-    console.log('this is return', retDate);
+
+    {retDate !== undefined ?
+      part1 = `${origin}-${airport}/${depDate}/${retDate}`
+      :
+      part1 = `${origin}-${airport}/${depDate}`;
+    }
     return (
-      <div className="flight-item">
+      <div className="flight-item"><a className="image-link"href={`https://www.kayak.com/flights/${part1}/?sort=price_a`}>
         <img className="airport-image" src={image}/>
-
-        <h3 className="city-name"> Airport Code: {airport}</h3>
-
-        <h3 className="city-name"> City: {city}</h3>
-
-        <h3 className="city-name"> Depart Time: {depDate}</h3>
-
-        <h3 className="city-name"> Price: {this.props.lowFareSearch.fare.total_price}</h3>
+        <h3 className="city-item-code">{airport}</h3>
+        <h3 className="city-item-name">{city}</h3>
+        <h3 className="city-depart"> Depart Time: {depDate}</h3>
+        <h3 className="city-price"> ${this.props.lowFareSearch.fare.total_price}</h3>
 
         {this.props.lowFareSearch.itineraries[0].inbound ?
-          <h3 className="city-name"> Return Date: {retDate}</h3>
+          <h3 className="city-return"> Return Date: {retDate}</h3>
           :
           undefined
         }
 
-        {retDate !== undefined ?
-          <a href={`https://www.kayak.com/flights/${origin}-${airport}/${depDate}/${retDate}/?sort=price_a`}>Go To Flight</a>
-          :
-          <a href={`https://www.kayak.com/flights/${origin}-${airport}/${depDate}/?sort=price_a`}>Go To Flight</a>
-        }
+      </a>
       </div>
     );
   }
