c67963dae4a41a8dad6fc3e70aa8d273de1ce2b5
Steven Carpenter <stevegcarpenter@gmail.com>
2018-03-22 23:34:50 -0700

    Removed some unused code

--- src/reducer/profile.js
+++ src/reducer/profile.js
@@ -2,13 +2,9 @@ export default (state = [], { type, payload }) => {
 
   switch (type) {
   case 'CLIENT_PROFILE_CREATE':
-    return payload;
   case 'CLIENT_PROFILE_ME':
-    return payload;
   case 'CLIENT_PROFILE_UPDATE':
     return payload;
-  case 'TOKEN_REMOVE':
-    return null;
   default:
     return state;
   }
