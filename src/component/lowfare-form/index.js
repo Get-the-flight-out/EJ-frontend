2cfbfb89ba24648402f9acd6aa0dce680bdf855a
heath smith <heathsmith73@gmail.com>
2018-04-04 15:02:27 -0700

    made the search for flights look for layovers also

--- src/component/lowfare-form/index.js
+++ src/component/lowfare-form/index.js
@@ -30,7 +30,7 @@ class LowFareForm extends React.Component {
         origin: '',
         destination: '',
         departure_date: '',
-        nonstop: true,
+        nonstop: false,
         max_price: '',
         return_date: '',
       }))
